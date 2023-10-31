160ff69996d4fd57e4c88f77941dec6b64f25f3e
Jared Patterson <jpwesley34@gmail.com>
2023-10-30 17:31:29 -0700

    does math

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
+const num3 = 24
+const num4 = 5
 
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 34) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 13, 19, 20);
 // Write your code here
